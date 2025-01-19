0e98e7b7fc57e65f11f676ea13c81a25c8526de2
Vincent Wong <vincentwong5609@gmail.com>
2025-01-18 17:38:42 -0800

    connected frontend to backend. Added cors to backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,33 +1,38 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
-import * as db from './db'
+import * as db from './db';
+import cors from 'cors';
 
 dotenv.config();
 
 const app: Express = express();
 
 app.use(express.json());
+app.use(cors());
 
 const port = process.env.PORT || 3000;
 
-app.post('/user', async (req: Request, res:Response) => {
-	const id = req.body.uid; 
+app.post('/user', async (req: Request, res: Response) => {
+	const id = req.body.uid;
 	const type = req.body.type;
 
 	try {
-		const userInfo = await db.getUser(id,type);
-		res.status(200).json({userInfo: userInfo, message: "User Found"});
+		const userInfo = await db.getUser(id, type);
+		res.status(200).json({ userInfo: userInfo, message: 'User Found' });
 	} catch (err) {
 		if (err instanceof Error) {
-			res.status(404).json({error: "Not Found", message: err.message});
+			res.status(404).json({ error: 'Not Found', message: err.message });
 		} else {
-			res.status(400).json({error: "Bad Request", message: "Unknown Error"});
+			res.status(400).json({
+				error: 'Bad Request',
+				message: 'Unknown Error',
+			});
 		}
-	} 
+	}
 });
 
-app.post('/newUser', async (req: Request, res:Response) => {
-	const id = req.body.uid; 
+app.post('/newUser', async (req: Request, res: Response) => {
+	const id = req.body.uid;
 	const user = req.body.username;
 	const firstName = req.body.firstName;
 	const lastName = req.body.lastName;
@@ -35,35 +40,54 @@ app.post('/newUser', async (req: Request, res:Response) => {
 	const password = req.body.password;
 	const type = req.body.type;
 	try {
-		const userInfo = await db.createUser(id,user,firstName,lastName,email,password,type);
-		res.status(200).json({userInfo: userInfo, message: `User ${user} Created`});
+		const userInfo = await db.createUser(
+			id,
+			user,
+			firstName,
+			lastName,
+			email,
+			password,
+			type
+		);
+		res.status(200).json({
+			userInfo: userInfo,
+			message: `User ${user} Created`,
+		});
 	} catch (err) {
 		if (err instanceof Error) {
-			res.status(400).json({error: "Bad Request", message: err.message});
+			res.status(400).json({
+				error: 'Bad Request',
+				message: err.message,
+			});
 		} else {
-			res.status(400).json({error: "Bad Request", message: "Unknown Error"});
+			res.status(400).json({
+				error: 'Bad Request',
+				message: 'Unknown Error',
+			});
 		}
 	}
-	
 });
 
 app.post('/event', async (req: Request, res: Response) => {
-	const id = req.body.uid; 
+	const id = req.body.uid;
 
 	try {
 		const eventInfo = await db.getEvent(id);
-		res.status(200).json({eventInfo: eventInfo, message: "Event Found"});
+		res.status(200).json({ eventInfo: eventInfo, message: 'Event Found' });
 	} catch (err) {
 		if (err instanceof Error) {
-			res.status(404).json({error: "Not Found", message: err.message});
+			res.status(404).json({ error: 'Not Found', message: err.message });
 		} else {
-			res.status(400).json({error: "Bad Request", message: "Unknown Error"});
+			res.status(400).json({
+				error: 'Bad Request',
+				message: 'Unknown Error',
+			});
 		}
-	} 
+	}
 });
 
 app.post('/newEvent', async (req: Request, res: Response) => {
-	const id = req.body.uid; 
+	const id = req.body.uid;
 	const title = req.body.title;
 	const location = req.body.location;
 	const start = req.body.start;
@@ -71,29 +95,57 @@ app.post('/newEvent', async (req: Request, res: Response) => {
 	const eoId = req.body.organizerId;
 
 	try {
-		const eventInfo = await db.createEvent(id,title, location,  start,end, eoId);
-		res.status(200).json({eventInfo: eventInfo, message: `Event ${title} Created`});
+		const eventInfo = await db.createEvent(
+			id,
+			title,
+			location,
+			start,
+			end,
+			eoId
+		);
+		res.status(200).json({
+			eventInfo: eventInfo,
+			message: `Event ${title} Created`,
+		});
 	} catch (err) {
 		if (err instanceof Error) {
-			res.status(400).json({error: "Bad Request", message: err.message});
+			res.status(400).json({
+				error: 'Bad Request',
+				message: err.message,
+			});
 		} else {
-			res.status(400).json({error: "Bad Request", message: "Unknown Error"});
+			res.status(400).json({
+				error: 'Bad Request',
+				message: 'Unknown Error',
+			});
 		}
 	}
 });
 
 app.post('/event_student', async (req: Request, res: Response) => {
-	const eventId = req.body.eventId; 
+	const eventId = req.body.eventId;
 	const studentId = req.body.studentId;
 
 	try {
-		const eventStudentInfo = await db.createEventStudent(eventId,studentId);
-		res.status(200).json({eventStudentInfo: eventStudentInfo, message: `Event ${eventId} for Student ${studentId} Created`});
+		const eventStudentInfo = await db.createEventStudent(
+			eventId,
+			studentId
+		);
+		res.status(200).json({
+			eventStudentInfo: eventStudentInfo,
+			message: `Event ${eventId} for Student ${studentId} Created`,
+		});
 	} catch (err) {
 		if (err instanceof Error) {
-			res.status(400).json({error: "Bad Request", message: err.message});
+			res.status(400).json({
+				error: 'Bad Request',
+				message: err.message,
+			});
 		} else {
-			res.status(400).json({error: "Bad Request", message: "Unknown Error"});
+			res.status(400).json({
+				error: 'Bad Request',
+				message: 'Unknown Error',
+			});
 		}
 	}
 });
@@ -103,32 +155,42 @@ app.get('/event_users/:eventId', async (req: Request, res: Response) => {
 
 	try {
 		const eventUsers = await db.getUsersByEvent(eventId);
-		res.status(200).json({eventUsers: eventUsers, message: `Students in Event ${eventId} Found`});
+		res.status(200).json({
+			eventUsers: eventUsers,
+			message: `Students in Event ${eventId} Found`,
+		});
 	} catch (err) {
 		if (err instanceof Error) {
-			res.status(404).json({error: "Not Found", message: err.message});
+			res.status(404).json({ error: 'Not Found', message: err.message });
 		} else {
-			res.status(400).json({error: "Bad Request", message: "Unknown Error"});
+			res.status(400).json({
+				error: 'Bad Request',
+				message: 'Unknown Error',
+			});
 		}
 	}
 });
-
 
 app.get('/user_events/:userId', async (req: Request, res: Response) => {
 	const userId = req.params.userId;
 
 	try {
 		const userEvents = await db.getEventsByUser(userId);
-		res.status(200).json({userEvents: userEvents, message: `Events for User ${userId} Found`});
+		res.status(200).json({
+			userEvents: userEvents,
+			message: `Events for User ${userId} Found`,
+		});
 	} catch (err) {
 		if (err instanceof Error) {
-			res.status(404).json({error: "Not Found", message: err.message});
+			res.status(404).json({ error: 'Not Found', message: err.message });
 		} else {
-			res.status(400).json({error: "Bad Request", message: "Unknown Error"});
+			res.status(400).json({
+				error: 'Bad Request',
+				message: 'Unknown Error',
+			});
 		}
 	}
 });
-
 
 app.listen(port, () => {
 	console.log(`[server]: Server is running at http://localhost:${port}`);
--- front-end/src/login.tsx
+++ front-end/src/login.tsx
@@ -1,84 +1,91 @@
-import React, {useState} from 'react';
-import {  signInWithEmailAndPassword   } from 'firebase/auth';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from './firebaseConfig';
-import { NavLink, useNavigate } from 'react-router-dom'
+import { NavLink, useNavigate } from 'react-router-dom';
 
 const Login = () => {
-    const navigate = useNavigate();
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+	const navigate = useNavigate();
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
 
-    const onLogin = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        signInWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-            // Signed in
-            const user = userCredential.user;
-            navigate("/home")
-            console.log(user);
-        })
-        .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            console.log(errorCode, errorMessage)
-        });
+	const onLogin = async (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		const details: { token?: string; id?: string } = {};
+		try {
+			const userCredential = await signInWithEmailAndPassword(
+				auth,
+				email,
+				password
+			);
+			const user = userCredential.user;
 
-    }
-    return(
-        <>
-            <main >        
-                <section>
-                    <div>                                            
-                        <p> FocusApp </p>                       
+			const accessToken = await user.getIdToken();
+			details['token'] = accessToken;
+			details['id'] = user.uid;
+			console.log(details);
 
-                        <form onSubmit={onLogin}>                                              
-                            <div>
-                                <label htmlFor="email-address">
-                                    Email address
-                                </label>
-                                <input
-                                    id="email-address"
-                                    name="email"
-                                    type="email"                                    
-                                    required                                                                                
-                                    placeholder="Email address"
-                                    onChange={(e)=>setEmail(e.target.value)}
-                                />
-                            </div>
+			const response = await axios.post('http://localhost:3000/user', {
+				details,
+			});
+			navigate('/');
+			console.log(response.data);
+			return response.data;
+		} catch (error) {
+			console.error(error);
+		}
+	};
 
-                            <div>
-                                <label htmlFor="password">
-                                    Password
-                                </label>
-                                <input
-                                    id="password"
-                                    name="password"
-                                    type="password"                                    
-                                    required                                                                                
-                                    placeholder="Password"
-                                    onChange={(e)=>setPassword(e.target.value)}
-                                />
-                            </div>
+	return (
+		<>
+			<main>
+				<section>
+					<div>
+						<p> FocusApp </p>
 
-                            <div>
-                                <button type="submit">      
-                                    Login                                                                  
-                                </button>
-                            </div>                               
-                        </form>
+						<form onSubmit={onLogin}>
+							<div>
+								<label htmlFor='email-address'>
+									Email address
+								</label>
+								<input
+									id='email-address'
+									name='email'
+									type='email'
+									required
+									placeholder='Email address'
+									onChange={(e) => setEmail(e.target.value)}
+								/>
+							</div>
 
-                        <p className="text-sm text-white text-center">
-                            No account yet? {' '}
-                            <NavLink to="/signup">
-                                Sign up
-                            </NavLink>
-                        </p>
+							<div>
+								<label htmlFor='password'>Password</label>
+								<input
+									id='password'
+									name='password'
+									type='password'
+									required
+									placeholder='Password'
+									onChange={(e) =>
+										setPassword(e.target.value)
+									}
+								/>
+							</div>
 
-                    </div>
-                </section>
-            </main>
-        </>
-    )
-}
+							<div>
+								<button type='submit'>Login</button>
+							</div>
+						</form>
 
-export default Login
+						<p className='text-sm text-white text-center'>
+							No account yet?{' '}
+							<NavLink to='/signup'>Sign up</NavLink>
+						</p>
+					</div>
+				</section>
+			</main>
+		</>
+	);
+};
+
+export default Login;
